916ce62bcdf21b7c2ea80f54a136b2b35bafde3e
1548976092 <1548976092@qq.com>
2019-04-17 19:34:50 -0700

    push.sh

--- make_kernel_src_need/make_kernel_src_need.sh
+++ make_kernel_src_need/make_kernel_src_need.sh
@@ -1,1 +1,5 @@
-sudo apt-get install u-boot-tools  swig 
+sudo apt-get install u-boot-tools  swig python2.7-dev
+sudo apt install debootstrap pv
+sudo apt-get install -y ccache
+sudo apt-get install binfmt-support
+sudo apt-get install liblz4-tool 
