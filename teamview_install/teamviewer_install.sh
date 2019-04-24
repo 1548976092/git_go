08d15dd422cfcb3163fe8fe1586c555a10abf123
1548976092 <1548976092@qq.com>
2019-04-23 18:44:26 -0700

    push.sh

--- teamview_install/teamviewer_install.sh
+++ teamview_install/teamviewer_install.sh
@@ -4,3 +4,13 @@ sudo apt install gdebi-core -y
 #安装包
 #sudo gdebi teamviewer_amd64.deb -y
 sudo gdebi teamviewer_13.2.26559_amd64.deb 
+
+
+
+#ubuntu杀毒软件
+http//www.comodo.com/home/internet-security/antivirus-for-linux.php
+
+sudo dpkg -i libssl0.9.8_0.9.8o-7ubuntu3.2.14.04.1_amd64.deb
+sudo dpkg -i cav-linux_x64.deb
+sudo  /opt/COMODO/post_setup.sh
+
