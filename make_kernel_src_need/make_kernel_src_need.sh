8fd2e199abaa4c70e10b9f2e53eb0d476192cf96
1548976092 <1548976092@qq.com>
2019-04-25 00:08:20 -0700

    push.sh

--- make_kernel_src_need/make_kernel_src_need.sh
+++ make_kernel_src_need/make_kernel_src_need.sh
@@ -21,7 +21,61 @@ Makefile有三个非常有用的变量。分别是$@，$^，$<代表的意义分
 $@--目标文件，$^--所有的依赖文件，$<--第一个依赖文件。 
 https://blog.csdn.net/YEYUANGEN/article/details/36898505
 
+1.原始gcc MAKEFILE
+gcc -c main.c 
+gcc -c mytool1.c 
+gcc -c mytool2.c 
+gcc -o main main.o mytool1.o mytool2.o
 
+2.
+main：main.o mytool1.o mytool2.o 
+gcc -o main main.o mytool1.o mytool2.o 
+main.o：main.c mytool1.h mytool2.h 
+gcc -c main.c 
+mytool1.o：mytool1.c mytool1.h 
+gcc -c mytool1.c 
+mytool2.o：mytool2.c mytool2.h 
+gcc -c mytool2.c
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+在Makefile中最重要的是描述文件的依赖关系的说明。
+格式:第一行表示的是依赖关系。第二行是规则。
+target：components 
+TAB rule 
+
+比如说我们上面的那个Makefile文件的第二行。 
+main：main.o mytool1.o mytool2.o 
+表示我们的目标(target)main的依赖对象(components)是main.o mytool1.omytool2.o 
+当倚赖的对象在目标修改后修改的话，就要去执行规则一行所指定的命令。
+就象我们的上面那个Makefile第三行所说的一样要执行 
+gcc-o main main.o mytool1.o mytool2.o 注意规则一行中的TAB表示那里是一个TAB键 
+
+Makefile有三个非常有用的变量。分别是$@，$^，$<代表的意义分别是： 
+$@--目标文件，$^--所有的依赖文件，$<--第一个依赖文件。 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+3。简化后的Makefile 
+main：main.o mytool1.o mytool2.o 
+gcc -o $@ $^ 
+main.o：main.c mytool1.h mytool2.h 
+gcc -c $< 
+mytool1.o：mytool1.c mytool1.h 
+gcc -c $< 
+mytool2.o：mytool2.c mytool2.h 
+gcc -c $< 
+
+4.经过简化后,我们的Makefile是简单了一点，不过人们有时候还想简单一点。这里我们学习一个Makefile的缺省规则 
+	.c.o： 
+	gcc -c $< 
+这个规则表示所有的 .o文件都是依赖与相应的.c文件的。例如mytool.o依赖于mytool.c这样Makefile还可以变为： 
+再次简化后的Makefile 
+	main：main.o mytool1.o mytool2.o 
+	gcc -o $@ $^ 
+	.c.o： 
+	gcc -c $< 
+
+
+**********************************************************************************************
 gcc编译参数：
 
 -S 预处理和编译，文件编译成为汇编代码                用法:gcc -S hello.c
