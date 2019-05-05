cb9ee7744c70d8308ce83b42e091a204f95240c2
1548976092 <1548976092@qq.com>
2019-05-04 23:14:10 -0700

    push.sh

---
+++ linuxcnc_install/linuxcnc_install.sh
@@ -0,0 +1,4 @@
+sudo apt install  python-pip python-tk  yapps2 libusb-1.0-0-dev libudev-dev libmodbus-dev libglib2.0-dev gnome-devel gnome-devel-docs tcl8.6-dev tk8.6-dev bwidget libtk-img tclx python-gtk2  libreadline-gplv2-dev  python-glade2 python-numpy  python-xlib python-gtkglext1 python-configobj python-gtksourceview2 libboost-python-dev python-opengl freeglut3-dev libxmu-dev
+
+
+
